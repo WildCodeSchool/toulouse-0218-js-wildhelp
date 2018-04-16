89d03f06b69cec2f9f0faabe24ead6b5f584ecaf
Florentin HAUTON <florentin.hauton@hotmail.fr>
2018-04-16 10:49:37 +0200

    MAJ presentation

--- formulaires.js
+++ formulaires.js
@@ -1,5 +1,6 @@
 'use strict'
 const targetElement = document.getElementById('main')
+// const mainDiv = document.getElementById('main')
 
 const accueilhtml =
  /* @html */`
@@ -278,6 +279,9 @@ const languageHtml = /* @html */`<div class="nav-side-menu">
       <nav class="my-2 my-md-0 mr-md-3">
          <a class="p-2 text-dark" href="/requete">Déposer une requête</a>
       </nav>
+      <nav class="my-2 my-md-0 mr-md-3">
+         <a class="p-2 text-dark" href="/aide">Demande d'aide</a>
+      </nav>
    </div>
 </div>
 <h1>Langages proposés</h1>
@@ -379,6 +383,9 @@ const coursproposeHtml = /* @html */ `<div class="nav-side-menu">
       <nav class="my-2 my-md-0 mr-md-3">
          <a class="p-2 text-dark" href="/requete">Déposer une requête</a>
       </nav>
+      <nav class="my-2 my-md-0 mr-md-3">
+         <a class="p-2 text-dark" href="/aide">Demande d'aide</a>
+      </nav>
    </div>
 </div>
 <a href="https://placeholder.com"><img class="banner" src="http://via.placeholder.com/1500x250" alt="banner"></a>
@@ -419,6 +426,9 @@ const listerequeteHtml = /* @html */ `<div class="nav-side-menu">
       </nav>
       <nav class="my-2 my-md-0 mr-md-3">
          <a class="p-2 text-dark" href="/cours">Voir les cours proposés</a>
+      </nav>
+      <nav class="my-2 my-md-0 mr-md-3">
+         <a class="p-2 text-dark" href="/aide">Demande d'aide</a>
       </nav>
    </div>
 </div>
@@ -469,124 +479,121 @@ const listerequeteHtml = /* @html */ `<div class="nav-side-menu">
    </div>
 </div>`
 
+
 // PAGE DEMANDE AIDE
-//
-// const mainDiv = document.getElementById('main')
-//
-// const homeHtml = /* @html */ `
-//     <div class="container">
-//         <form id="formHelp" class="form-horizontal" method="POST" action="/ma-demande-de-cours">
-//             <div class="row">
-//                 <div class="col-md-12">
-//                     <h2>Demande d'aide</h2>
-//                 </div>
-//             </div>
-//             <div class="row">
-//                 <div class="col-md-12 ">
-//                     <div class="input-group mb-2 mr-sm-2 mb-sm-0">
-//                         <div class="input-group-addon" style="width: 2.6rem">
-//                             <i class="fa fa-repeat"></i>
-//                         </div>
-//                         <select id="subject" name="subject" class="form-control" required="required">
-//                             <option value="" selected="">Selectionnez le langage</option>
-//                             <option value="JavaScriptAngular">JavaScript Angular</option>
-//                             <option value="JavaScriptReact">JavaScript React</option>
-//                             <option value="JavaScriptVue">JavaScript Vue.js</option>
-//                             <option value="JavaJ2EE">JAVA J2EE</option>
-//                             <option value="JavaAndroid">JAVA Android</option>
-//                             <option value="PHP">PHP</option>
-//                             <option value="Symfonyt">Symfony</option>
-//                         </select>
-//                     </div>
-//                 </div>
-//             </div>
-//             <div class="row">
-//                 <div class="col-md-12">
-//                     <p>
-//                         <label for="sujet">Sujet requête :</label>
-//                         <input type="text" name="theme" id="theme" placeholder="Ex : Algorithme" size="40" maxlength="50" required="required" />
-//                     </p>
-//
-//                 </div>
-//             </div>
-//             <div class="row">
-//                 <div class="col-md-12">
-//                     <p>
-//                       <textarea name="ameliorer" id="ameliorer" rows="10" cols="50" required="required"
-//                       placeholder="Décris ici en détail ton problème"></textarea>
-//                     </p>
-//                 </div>
-//             </div>
-//             <div class="row">
-//               <div class="col-md-6">
-//                 <input class="btn btn-outline-primary" type="button" value="Save me !" data-toggle="modal" data-target="#myModal"/>
-//               </div>
-//               <div class="col-md-6">
-//                 <input class="btn btn-outline-danger" type="reset" value="Annuler" />
-//               </div>
-//             </div>
-//             <!-- The Modal -->
-//           <div class="modal fade" id="myModal">
-//             <div class="modal-dialog">
-//               <div class="modal-content">
-//                 <div class="modal-header">
-//                   <h4 class="modal-title">Êtes-vous sûr ?</h4>
-//                   <input type="button" class="close" data-dismiss="modal">&times;</input>
-//                 </div>
-//                 <div class="modal-body">
-//                   Validez pour confirmer votre demande !
-//                 </div>
-//                 <div class="modal-footer">
-//                   <input type="submit" class="btn btn-primary" data-dismiss="modal" value="Valider"></input>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         </form>
-//     </div>`
-//
-//
-//
-//
-// const render = mainHtml => {
-//     mainDiv.innerHTML = mainHtml
-// }
-//
-// const home = () => {
-//     render(homeHtml)
-//
-//     const formCours = document.getElementById('formHelp')
-//     formCours.addEventListener('submit', event => {
-//
-//       let data = {}
-//
-//       event.preventDefault()
-//       const inputs = formCours.getElementsByTagName('input')
-//       for(input of inputs) {
-//       if(input.name !== '') {
-//         console.log(input.name)
-//         data[input.name] = input.value
-//         }
-//       }
-//
-//       const body = JSON.stringfly(data)
-//       console.log(body)
-//
-//       fetch('/ma-demande-de-cours', {
-//         method: 'POST',
-//         body: body,
-//         headers: {
-//           Accept: 'application/json',
-//           'Content-Type': 'application/json'
-//         }
-//       })
-//       .then(response => response.json())
-//       .then(data => {
-//         console.log(data)
-//       })
-//     })
-// }
-// home()
+
+const aideHtml = /* @html */ `
+    <div class="container">
+        <form id="formHelp" class="form-horizontal" method="POST" action="/aide">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Demande d'aide</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 ">
+                    <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+                        <div class="input-group-addon" style="width: 2.6rem">
+                            <i class="fa fa-repeat"></i>
+                        </div>
+                        <select id="subject" name="subject" class="form-control" required="required">
+                            <option value="" selected="">Selectionnez le langage</option>
+                            <option value="JavaScriptAngular">JavaScript Angular</option>
+                            <option value="JavaScriptReact">JavaScript React</option>
+                            <option value="JavaScriptVue">JavaScript Vue.js</option>
+                            <option value="JavaJ2EE">JAVA J2EE</option>
+                            <option value="JavaAndroid">JAVA Android</option>
+                            <option value="PHP">PHP</option>
+                            <option value="Symfonyt">Symfony</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <p>
+                        <label for="sujet">Sujet requête :</label>
+                        <input type="text" name="theme" id="theme" placeholder="Ex : Algorithme" size="40" maxlength="50" required="required" />
+                    </p>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <p>
+                      <textarea name="ameliorer" id="ameliorer" rows="10" cols="50" required="required"
+                      placeholder="Décris ici en détail ton problème"></textarea>
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+              <div class="col-md-6">
+                <input class="btn btn-outline-primary" type="button" value="Save me !" data-toggle="modal" data-target="#myModal"/>
+              </div>
+              <div class="col-md-6">
+                <input class="btn btn-outline-danger" type="reset" value="Annuler" />
+              </div>
+            </div>
+            <!-- The Modal -->
+          <div class="modal fade" id="myModal">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h4 class="modal-title">Êtes-vous sûr ?</h4>
+                  <input type="button" class="close" data-dismiss="modal">&times;</input>
+                </div>
+                <div class="modal-body">
+                  Validez pour confirmer votre demande !
+                </div>
+                <div class="modal-footer">
+                  <input type="submit" class="btn btn-primary" data-dismiss="modal" value="Valider"></input>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+    </div>`
+
+
+
+
+
+
+const aide = () => {
+    render(aideHtml)
+
+    const formCours = document.getElementById('formHelp')
+    formCours.addEventListener('submit', event => {
+
+      let data = {}
+
+      event.preventDefault()
+      const inputs = formCours.getElementsByTagName('input')
+      for(input of inputs) {
+      if(input.name !== '') {
+        console.log(input.name)
+        data[input.name] = input.value
+        }
+      }
+
+      const body = JSON.stringfly(data)
+      console.log(body)
+
+      fetch('/aide', {
+        method: 'POST',
+        body: body,
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+      })
+    })
+}
+
 
 
 const footerForAllPage = /* @html */ `
@@ -640,6 +647,10 @@ const footerForAllPage = /* @html */ `
     render(listerequeteHtml)
   }
 
+  const showAide = () => {
+    render(aideHtml)
+  }
+
 page('/', showAccueil)
 page('/connexion', showConnexion)
 page('/wilder', showInscriptionWilder)
@@ -647,4 +658,5 @@ page('/helper', showInscriptionHelper)
 page('/languages', showLanguages)
 page('/cours', showCoursPropose)
 page('/requete', showListeRequete)
+page('/aide', showAide)
 page()
