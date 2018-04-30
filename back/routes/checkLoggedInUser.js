a3373135ac0e89267fc9bcb2cf6a03fac6d999f2
Leila DQIQI <leila.dqiqi@yahoo.fr>
2018-04-30 09:41:21 +0200

    test refaire la connexion moi meme

--- back/routes/checkLoggedInUser.js
+++ back/routes/checkLoggedInUser.js
@@ -1,15 +1,16 @@
 const  connection = require('../db')
 const mysqlEscape = require('../mysqlEscape')
-// const session = require('express-session')
-// app.use(session({secret:"cats", resave:true, saveUninitialized: true}))
-// app.use(bodyParser.json())
-// app.use(bodyParser.urlencoded({extended: true}))
+const session = require('express-session')
+app.use(session({secret:"cats", resave:true, saveUninitialized: true}))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
-const checkLoggedInUser = (req,res) => {
+const checkLoggedInUser = (req,res,next) => {
   const {email, password} = req.body
   console.log(req.body)
   if (!email || !password){
     return res.status(400).json({error: 'champs non remplis'})
+    
   }
 
   const userConnexion = `SELECT * FROM user WHERE email='${req.body.email}' AND password='${req.body.password}'`
