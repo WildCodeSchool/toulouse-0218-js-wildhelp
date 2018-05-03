3946f42b178e75b30a600ff35a7c649f50d21124
Florentin HAUTON <florentin.hauton@hotmail.fr>
2018-05-03 16:59:12 +0200

    tentative de test

--- back/routes/sendMails.js
+++ back/routes/sendMails.js
@@ -3,22 +3,24 @@ const nodemailer = require('nodemailer')
 
 // messagerie
 let smtpConfig = {
-    host: 'flokri14@gmail.com',
-    port: 587,
-    secure: false, // upgrade later with STARTTLS
+  service: 'gmail',
+    // host: 'smtp.gmail.com',
+    // port: 587,
+    // secure: false, // upgrade later with STARTTLS
     auth: mailSettings
 }
 
 let mailOptions = {
-  from: 'florentin.hauton@hotmail.fr'
-  to: 'myfriend@yahoo.com, myotherfriend@yahoo.com',
+  to: 'florentin.hauton@hotmail.fr',
+  from: 'flokri14@gmail.com',
   subject: 'Un Alumni vous propose son aide !',
-  text: 'Bonjour Wilder, un Alumni vous vpropose son aide'
+  text: 'Bonjour Wilder, un Alumni vous vpropose son aide',
   html: '<h1>Welcome</h1><p>That was easy!</p>'
 }
 
 
 // test de la connexion et de l'authentification
+const transporter = nodemailer.createTransport(smtpConfig)
 transporter.sendMail(mailOptions, function(error, info) {
   if (error) {
     console.log(error)
@@ -27,5 +29,3 @@ transporter.sendMail(mailOptions, function(error, info) {
     console.log('Email sent: ' + info.response)
   }
 })
-
-}
